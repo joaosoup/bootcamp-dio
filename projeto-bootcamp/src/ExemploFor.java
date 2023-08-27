2444d13a6bc5582f5c603e13b4f3e45fdc26cbb8
João Victor <hauntermonster68@gmail.com>
2023-08-26 22:16:31 -0300

    Revisão: Usando For em Strings

---
+++ projeto-bootcamp/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        for(int carneirinhos = 2; carneirinhos <= 20; carneirinhos++){
+        System.out.println("Acabaram de pular " + carneirinhos + " carneirinhos pela cerca.");
+        }
+        System.out.println("Boa noite!");
+    }
+    
+}
