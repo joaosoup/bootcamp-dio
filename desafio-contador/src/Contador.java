c81db62a50dcdf6cfc9c73ae5970d0d8a1af744e
João Victor <hauntermonster68@gmail.com>
2023-09-19 10:02:16 -0300

    Término: Desafio Contador com exceções

--- desafio-contador/src/Contador.java
+++ desafio-contador/src/Contador.java
@@ -5,6 +5,23 @@ public class Contador {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o primeiro número inteiro");
         int numero1 = scanner.nextInt();
-        
+        System.out.println("Digite o segundo número inteiro");
+        int numero2 = scanner.nextInt();
+        scanner.close();
+
+        try {
+            if (numero2 <= numero1){
+                throw new ParametrosInvalidosException ("O segundo parâmetro deve ser maior que o primeiro");
+            }
+
+        int quantFor = numero2 - numero1;
+
+        for (int i = 0; i < quantFor; i++  ){
+            System.out.println("Imprimindo o número " + (i + 1));
+            
+        }
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
     }
 }
