73a74dd2328a7f5ef384ff6deb3bb9506f1ecd39
João Victor <hauntermonster68@gmail.com>
2023-09-18 22:23:25 -0300

    Finalização: Projeto Processo seletivo

--- cases-java-basic/src/candidatura/ProcessoSeletivo.java
+++ cases-java-basic/src/candidatura/ProcessoSeletivo.java
@@ -1,9 +1,13 @@
 package candidatura;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
+        
         selecaoCandidatos();
     }
 
@@ -11,47 +15,63 @@ public class ProcessoSeletivo {
         String[] candidatos = { "FELIPE", "MÁRCIA", "JULIA", "PAULO", "AUGUSTO", "MÔNICA", "FABRÍCIO", "MIRELA",
                 "DANIELA", "JORGE" };
 
-        String[] candidatosSelecionados = new String[5]; 
-        for (int i = 0; i < candidatos.length; i++) {
-            System.out.println("O candidato de index " + (i + 1) + " é " + candidatos[i]);
-        }
+        String[] candidatosSelecionados = new String[5];
 
         int candidatosSelecionadosCount = 0;
-        int candidatoAtual = 0; // indice Array
+        int candidatoAtual = 0; // Índice do Array
         double salarioBase = 2000.0;
         while (candidatosSelecionadosCount < 5 && candidatoAtual < candidatos.length) {
             String candidato = candidatos[candidatoAtual];
             double salarioPretendido = valorPretendido();
 
-            System.out.println("O candidato " + candidato + " Solicitou esse valor de salário: " + salarioPretendido);
+            Locale localeBR = new Locale("pt","BR");
+            NumberFormat dinheiro = NumberFormat.getCurrencyInstance(localeBR);
+
+
+
+            System.out.println("O candidato " + candidato + " solicitou esse valor de salário: " + dinheiro.format(salarioPretendido));
 
             if (salarioBase >= salarioPretendido) {
                 candidatosSelecionados[candidatosSelecionadosCount] = candidato;
-                System.out.println("O candidato " + candidato + " Foi selecionado para a vaga.");
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
                 candidatosSelecionadosCount++;
             }
             candidatoAtual++;
-
         }
 
-        System.out.println("\nCandidatos selecionados para a vaga:");
+        System.out.println("\nCandidatos selecionados para a vaga: ");
         for (int i = 0; i < candidatosSelecionadosCount; i++) {
-            System.out.println(candidatosSelecionados[i]);
+            System.out.println("O candidato(a) " + candidatosSelecionados[i] + " foi selecionado");
+
+            boolean atendeu = fazerLigacao(candidatosSelecionados[i]); 
+            if (atendeu) {
+                System.out.println("Conseguimos contato com " + candidatosSelecionados[i]);
+            } else {
+                System.out.println("Não conseguimos contato com " + candidatosSelecionados[i]);
+            }
         }
+    }
+
+    static boolean fazerLigacao(String candidato) {
+        int tentativas = 0;
+        while (tentativas < 3) {
+            boolean atendeu = atender(); 
+            System.out.println("Ligando para o candidato " + candidato + " - Tentativa " + (tentativas + 1));
+            if (atendeu) {
+                return true; 
+            }
+            tentativas++;
+        }
+        return false; 
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
     }
 
     static double valorPretendido() {
+        
+        
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
-    }
-
-    static void analisarCandidato(double salarioPretendido) {
-        double salarioBase = 2000;
-        if (salarioBase > salarioPretendido) {
-            System.out.println("Ligar para candidato");
-        } else if (salarioBase == salarioPretendido) {
-            System.out.println("Ligar para o candidato com contra-proposta");
-        } else {
-            System.out.println("Aguardando resultado dos demais candidatos");
-        }
     }
 }
