edf896c30572fd3de0f3ea50a4928c7a34bc8b1e
João Victor <hauntermonster68@gmail.com>
2023-09-18 22:33:31 -0300

    Adição: Pasta do desafio do contador

---
+++ desafio-contador/src/Contador.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número inteiro");
+        int numero1 = scanner.nextInt();
+        
+    }
+}
